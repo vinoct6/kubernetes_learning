98202ef1b3a538dc8b8d7c72aa3ca923084aac01
VinothKumar <vinoth.mani2@wipro.com>
2019-09-18 07:48:13 +0100

    Adds ConfigMaps

--- deploy_in_cloud.sh
+++ deploy_in_cloud.sh
@@ -1,4 +1,6 @@
 kubectl apply -f ./storage.yaml
 kubectl apply -f ./mongo-stack.yaml
+kubectl apply -f ./config-map.yaml
+kubectl apply -f ./config-map-multiline.yaml
 kubectl apply -f ./workloads.yaml
 kubectl apply -f ./services.yaml
